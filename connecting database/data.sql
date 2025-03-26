d09cbfff4a5b7b0336284bf7c1ec79a422ea2bd2
Rayella Srujan teja <64093603+sr376@users.noreply.github.com>
2025-03-26 10:43:08 +0530

    Create data.sql

---
+++ connecting database/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO "ALIEN" (id, name, tech) VALUES
+                                         (101, 'vivek', 'chain'),
+                                         (102, 'srujan', 'java'),
+                                         (103, 'teja', 'python');
