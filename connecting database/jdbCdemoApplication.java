3eb45e236beedc0b9522f3acba4357e0c520265e
Rayella Srujan teja <64093603+sr376@users.noreply.github.com>
2025-03-26 10:12:33 +0530

    Create jdbCdemoApplication.java

---
+++ connecting database/jdbCdemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.JDBCdemo;
+
+import com.example.JDBCdemo.model.Alien;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class JdbCdemoApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = SpringApplication.run(JdbCdemoApplication.class, args);
+		Alien alien1 = context.getBean(Alien.class);
+		alien1.setId(111);
+		alien1.setName("srujan");
+		alien1.setTech("java");
+
+		AlienRepo repo = context.getBean(AlienRepo.class);
+		repo.save(alien1); // Fixed issue
+		System.out.println(repo.findAll());
+	}
+}
