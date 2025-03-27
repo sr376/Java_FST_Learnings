fb61a3dff73ff29eb9f56172609955bf1cee6416
Rayella Srujan teja <64093603+sr376@users.noreply.github.com>
2025-03-27 09:24:30 +0530

    Create HomeContoller1.java

---
+++ SpringMVC/HomeContoller1.java
@@ -0,0 +1,17 @@
+public class HomeController
+{
+  @RequestMapping("/")
+  public String home(){
+    return "index.jsp";
+  }
+  @RequestMapping("add")
+  public String add(HttpServletRequest req){
+    int i = Integer.parseInt(req.getParameter("num1"));
+    int j = Integer.parseInt(req.getParameter("num2"));
+    int num3 = i+j;
+    HttpSession session = req.getSession();
+    session.setAttribute("num3", num3);
+    return "result.jsp";
+  }
+}
+
