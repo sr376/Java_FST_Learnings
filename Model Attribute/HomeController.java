2497640860ba3eb612a9fa6ae2243b9cd7df121e
Rayella Srujan teja <64093603+sr376@users.noreply.github.com>
2025-03-28 16:43:17 +0530

    Update HomeController.java

--- Model Attribute/HomeController.java
+++ Model Attribute/HomeController.java
@@ -1,24 +1,24 @@
-
 @Controller
-public class HomeController
-{
-@RequestMapping("/")
-public String home()
-{
-return "index";
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @RequestMapping("add")
+    public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, Model m) {
+        int num3 = i + j;
+        m.addAttribute("num3", num3);
+        return "result";
+    }
+
+    @RequestMapping("addAlien")
+    public String addAlien(@RequestParam("aid") int aid, @RequestParam("aname") String aname, Model m) {
+        Alien a = new Alien();
+        a.setAid(aid);
+        a.setAname(aname);
+        m.addAttribute("alien", a);
+        return "result";
+    }
 }
-@RequestMapping("add")
-public String add (@RequestParam("num1") int i,@RequestParam("num2") int j, Model m)
-{
-int num3i+ j;
-m.addAttribute("num3", num3);
-return "result";
-}
-I.
-@RequestMapping ("addAlien")
-public String addAlien (Alien al, Model m)
-{
-}
-}
-m.addAttribute("alien", a);
-return "result";
