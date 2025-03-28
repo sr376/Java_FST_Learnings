462d6c137b4eb87c4e9a5df253841aa006e3f307
Rayella Srujan teja <64093603+sr376@users.noreply.github.com>
2025-03-28 17:27:44 +0530

    Create result3.js

---
+++ Model Attribute/result3.js
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" isELIgnored="false" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Result Page</title>
+</head>
+<body>
+    <h2>Result:</h2>
+    <p>Alien Details: ${a1}</p>
+    <p>Welcome Back, ${name}</p>
+</body>
+</html>
